20375d44318d463d90433cb73b89f1e6d32c9ebb
彭子修 <pengzixiu@pengzixiudeAir.lan>
2018-07-21 08:12:46 +0800

    add

--- MiniTag/index.jsx
+++ MiniTag/index.jsx
@@ -5,41 +5,66 @@ export default class MiniTag extends React.Component{
     constructor(props){
         super(props)
         this.state = {
-            isDraging: false
+            isDraging: false,
+            contentIsShow: false
         }
     }
 
   componentDidMount() {
+    // 初始化可拖拽浮窗
     this.dragable_MiniTag();
+    // 初始化弹窗
     this.miniWindow();
+    // 初始化弹窗的图片
+    this.show_mini_tag_img();
+  }
+
+  show_mini_tag_img = () => {
+    let img = this.refs.mini_tag_button_img;
+    img ? img.style.opacity = 1 : null;
+    let contentImg = this.refs.mini_tag_content_img;
+    contentImg ? contentImg.style.opacity = 1 : null;
   }
 
   miniWindow = () => {
     let btn = document.getElementById('mini_tag_button');
     let content = document.getElementById('mini_tag_content');
+    let inside_content = document.getElementById('mini_tag_content_inside_container');
+    let close_mini_content_btn = document.getElementById('close_mini_tag_btn');
 
     btn.addEventListener('touchend', ()=>{
-        console.log(this.state.isDraging)
         if(this.state.isDraging === false){
-            console.log(this.state.isDraging)
-
-            let W = document.documentElement.clientWidth || document.body.clientWidth;
-            let H = document.documentElement.clientHeight || document.body.clientHeight;
-            console.log('ceshi');
-            console.log(W);
-            console.log(H);
+            btn.style.webkitAnimationPlayState = 'paused';
+            btn.style.zIndex = 1;
+            content.style.display = 'block';
             content.style.zIndex = 9999;
-            content.style.height = H + 'px';
-            // content.style.outline = '256px solid rgba(0, 0, 0, .5)';   
-            window.requestAnimationFrame(this.AniMove);
+            setTimeout(()=>{
+                content.style.visibility = 'visible';
+                content.style.top = '0';
+                content.style.opacity = 1; 
+                content.style.backgroundColor = 'rgba(0, 0, 0, .54)';
+                this.setState({contentIsShow: true})
+                window.requestAnimationFrame(this.AniMove);
+                window.requestAnimationFrame(this.scrollToTop);
+            }, 100)
         }else{
             return 
         }
     })
 
-    content.addEventListener('click', ()=>{
+    content.addEventListener('click', (e)=>{
+        e.preventDefault();
+        btn.style.webkitAnimationPlayState = 'running';
+        btn.style.zIndex = 9999;
         content.style.zIndex = 1;
-        content.style.height = 0 + 'px';
+        content.style.top = '100vh';
+        content.style.opacity = 0;
+        content.style.visibility = 'hidden';
+        content.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+        setTimeout(()=>{
+            content.style.display = 'none';
+            this.setState({contentIsShow: false})
+        }, 100)
     })
 
   }
@@ -56,7 +81,17 @@ export default class MiniTag extends React.Component{
         let touchX = Number(touch.pageX);
         let touchY = Number(touch.pageY);
 
-        window.addEventListener('touchmove', (e)=>{
+        btn.addEventListener('touchmove', (e)=>{
+            if(this.state.contentIsShow){
+                return
+            }
+            // console.log(btnX, touchX);
+            if(touchX < btnX || touchX > (btn.offsetWidth + btnX)){
+                return 
+            }
+            if(touchY < btnY || touchY > (btn.offsetHeight + btnY)){
+                return 
+            }
             this.setState({isDraging: true})
             btn.style.cursor = 'move';
             let new_touch = e.touches[0];
@@ -101,17 +136,42 @@ export default class MiniTag extends React.Component{
     })
   }
 
+  scrollToTop = () => {
+    let currentTop = document.documentElement.scrollTop || document.body.scrollTop;
+    console.log(currentTop)
+    if(currentTop > 0){
+        let speed = parseInt(currentTop / 10);
+        window.scrollTo(0, speed);
+        window.requestAnimationFrame(this.scrollToTop);
+    }else{
+        return
+    }
+  }
+
   AniMove = () => {
     window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
     let btn = document.getElementById('mini_tag_button');
     let contentArea = document.documentElement.clientWidth || document.body.clientWidth;
     let btnx = btn.offsetLeft;
 
-    if(btnx > parseInt(contentArea / 2) && btnx < parseInt(contentArea - btn.offsetWidth)){
+    if(btnx >= parseInt(contentArea / 2) && btnx < parseInt(contentArea - btn.offsetWidth)){
         let speed = parseInt((contentArea - btnx)/10)
-        btn.style.left = btnx + speed + 'px';
+
+        if((btnx + speed) > parseInt(contentArea - btn.offsetWidth)){
+            btn.style.left = parseInt(contentArea - btn.style.left) + 'px'
+            return 
+        }else{
+            btn.style.left = parseInt(btnx + speed) + 'px';
+        }
+        // console.log(speed)
+        // if(btn.style.left > parseInt(contentArea - btn.offsetWidth)){
+        //     console.log(parseInt(contentArea - btn.offsetWidth));
+        //     btn.style.left = parseInt(contentArea - btn.offsetWidth)
+        //     console.log(btn.style.left, btn.offsetLeft);
+        // }
+        // console.log(contentArea - btn.offsetWidth)
         window.requestAnimationFrame(this.AniMove);
-    }else if(btnx < parseInt(contentArea / 2) && btnx > 0){
+    }else if(btnx <= parseInt(contentArea / 2) && btnx > 0){
         // 注意速度0<speed<1
         let speed = Math.ceil(btnx / 10)
         btn.style.left = btnx - speed + 'px';
@@ -126,12 +186,23 @@ export default class MiniTag extends React.Component{
     return (
       <div className="mini_tag">
 
-            <div id='mini_tag_button' className="mini_tag_button">
-                <img src={require('../../../assets/images/propose.jpg')} alt="mini_tag"/>
+            <div id='mini_tag_button' className="mini_tag_button mini_tag_button_ripple">
+                <img ref='mini_tag_button_img' style={{opacity: 0}} src={require('../../../assets/images/minitag_icon.jpg')} alt="mini_tag"/>
             </div>
             
             <div id='mini_tag_content' className="mini_tag_content">
-                <img src={require('../../../assets/images/miniPage.jpg')} alt=""/>
+
+            <div
+                id='mini_tag_content_inside_container'
+                className='mini_tag_content_inside_container'
+            >
+                <div className="content_img_container">
+                    <img ref='mini_tag_content_img' src={require('../../../assets/images/mini_page.jpg')} style={{opacity: 0}} alt=""/>
+                </div>
+            </div>
+        
+            <img id="close_mini_tag_btn" src={require('../../../assets/images/close.svg')}  alt=""/>
+
             </div>
 
       </div>
---
+++ MiniTag/wrap_index.jsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import Loadable from 'react-loadable';
+
+// const LoadableBar = Loadable({
+//   loader: () => import('./index.jsx'),
+//   loading() {
+//     return <div>Loading...</div>
+//   }
+// });
+
+// export default class WrapMiniTag extends React.Component {
+//   render() {
+//     return <LoadableBar/>;
+//   }
+// }
